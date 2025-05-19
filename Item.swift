ad4c054cdf6ee4afb77df579e8b35330ac7eab44
Miguel Ruiz <miruhe1995@gmail.com>
2025-05-18 22:51:49 -0500

    Initial commit

---
+++ Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CookLab
+//
+//  Created by Miguel Ruiz on 18/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
