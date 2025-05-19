99e2c9cac1cae35ffafdf98707853aa3d44d777d
Miguel Ruiz <miruhe1995@gmail.com>
2025-05-18 23:14:24 -0500

    Organize folders and files, and rename View

--- CookLabApp.swift
+++ CookLabApp.swift
@@ -25,7 +25,7 @@ struct CookLabApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RecipesListView()
         }
         .modelContainer(sharedModelContainer)
     }
