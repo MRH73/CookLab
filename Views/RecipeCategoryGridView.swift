ec39a79240d27167939e284d9f286ab8a3892260
Miguel Ruiz <miruhe1995@gmail.com>
2025-05-23 13:17:54 -0500

    Create RecipeCategoryGridView.swift

---
+++ Views/RecipeCategoryGridView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCategoryGridView.swift
+//  CookLab
+//
+//  Created by Miguel Ruiz on 23/5/25.
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeCategoryGridView()
+}
